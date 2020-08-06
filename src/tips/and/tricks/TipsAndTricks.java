c81e7e695cd91875e2f778c2dc1b258d1a88ac9d
shyamzzp <shyamsundersuthar009@gmail.com>
2020-08-07 01:15:04 +0530

    Add snippet to get the number of digits in a number.

--- src/tips/and/tricks/TipsAndTricks.java
+++ src/tips/and/tricks/TipsAndTricks.java
@@ -15,7 +15,8 @@ public class TipsAndTricks {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        long number = 1122255222;
+        System.out.println(Math.floor(Math.log10(number)) + 1);
     }
     
 }
